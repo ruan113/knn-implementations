2726b1e9a6ba266d0979b4f7954ff549b96bd06a
Ruan Michel Adabo <ruanbento@gmail.com>
2021-03-22 14:26:54 -0300

    [REFACTOR] Implementação de metricas para comparação

--- minha-versao/main.py
+++ minha-versao/main.py
@@ -1,9 +1,9 @@
 # from knnCrispy import CrispyKNN
 from knnFormated import CrispyKNN
 from knnFuzzyFormated import FuzzyKNN
-from pprint import pprint
-from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.datasets import load_iris, load_breast_cancer, load_diabetes
+from utils import getClasses
+from sklearn.metrics import confusion_matrix, classification_report, plot_confusion_matrix
+import pandas as pd
 
 from utils import getData
 
@@ -12,12 +12,16 @@ from utils import getData
 # diabetes = load_diabetes()
 # dataset = diabetes
 
-dataset = getData('minha-versao/data-sets/adult-data-set/adult_1k.data' , 0.75)
+# dataset = getData('minha-versao/data-sets/adult-data-set/adult_1k.data' , 0.75)
+dataset = getData('minha-versao/data-sets/iris-data-set/iris_full.data' , 0.75)
 
 xTrain = dataset['training']['data']
 yTrain = dataset['training']['target']
 xTest = dataset['test']['data']
 yTest = dataset['test']['target']
+
+# print(yTrain)
+# print(yTest)
 
 # Passar valor como parametro caso queira valor de k diferente de 3
 skModel = CrispyKNN()
@@ -28,16 +32,47 @@ skModel.fit(xTrain, yTrain)
 custModel.fit(xTrain, yTrain)
 
 try:
-  print(cross_val_score(cv=5, estimator=skModel, X=xTest, y=yTest))
-  print(cross_val_score(cv=5, estimator=custModel, X=xTest, y=yTest))
+  # print(cross_val_score(cv=5, estimator=skModel, X=xTest, y=yTest))
+  # print(cross_val_score(cv=5, estimator=custModel, X=xTest, y=yTest))
+  # print([t[0] for t in skModel.predict(xTest)])
+  # print(len(yTest))
+  # print(len([t[0] for t in skModel.predict(xTest)]))
+  # print(dataset['complete']['target'])
+  # print(getClasses(dataset['complete']['target']))
+
+  # print(list(yTest))
+  # print([t[0] for t in skModel.predict(xTest)])
+
+  knnReport = classification_report(
+    list(yTest), 
+    [t[0] for t in skModel.predict(xTest)], 
+    labels=getClasses(dataset['complete']['target']),
+    zero_division=0
+  )
+  knnConfusionMatrix = confusion_matrix(
+    list(yTest), 
+    [t[0] for t in skModel.predict(xTest)], 
+    labels=getClasses(dataset['complete']['target'])
+  )
+  fknnReport = classification_report(
+    list(yTest), 
+    [t[0] for t in custModel.predict(xTest)], 
+    labels=getClasses(dataset['complete']['target']),
+    zero_division=0
+  )
+  fknnConfusionMatrix = confusion_matrix(
+    list(yTest), 
+    [t[0] for t in custModel.predict(xTest)], 
+    labels=getClasses(dataset['complete']['target'])
+  )
+
+  print('-------------------------- Knn --------------------------')
+  print(knnReport)
+  print('matriz de confunsão\n')
+  print(knnConfusionMatrix)
+  print('-------------------------- FKnn --------------------------')
+  print(fknnReport)
+  print('matriz de confunsão\n')
+  print(fknnConfusionMatrix)
 except:
   print('Fail to cross validate!')
-
-
-# porcentagem = 0.85 # porcentagem de dados que será usada como instancia de treinamento
-# k = 3 # num de vizinhos que serão considerados
-
-# knn('minha-versao/data-sets/iris-data-set/iris_full.data', porcentagem, 5, k)
-# knn('minha-versao/data-sets/adult-data-set/adult_1k.data', porcentagem, 14, k)
-# knn('minha-versao/data-sets/adult-data-set/adult_5k.data', porcentagem, 14, k)
-# knn('minha-versao/data-sets/adult-data-set/adult_10k.data', porcentagem, 14, k)
